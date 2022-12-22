c2c45ba8aa1a659e70f12af96c8f828281671a91
Kamilah <k.muhammad95@gmail.com>
2022-12-22 11:19:05 -0600

    exercise completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName = "Kamilah";
+            int myAge = 27;
+            char myInitial = 'K';
+            bool isFunny = true;
+            double myWeight = 140;
+            decimal myHeight = 5.25m;
+
+            Console.WriteLine($"Hello {firstName}. You are {myAge} years old. You are {myHeight}.");
+
         }
     }
 }
